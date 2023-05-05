3d114f848b6b9ea5941e2ad9a3852cacb840deb1
christian <criszdev@gmail.com>
2023-05-05 10:38:06 -0300

    reorganized pages files

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
-import Navbar from "../components/Navbar"
-import Button from "../components/Button"
-import planet from "../assets/planet.png"
+import Navbar from "../../components/Navbar/Navbar"
+import Button from "../../components/Button/Button"
+import planet from "../../assets/planet.png"
+import { Link } from "react-router-dom"
+import './home.css'
 
 function Home() {
   return (
@@ -9,7 +11,7 @@ function Home() {
       <section className="container">
         <div className="title-section">
           <h1 className="title">React Planet Website</h1>
-          <Button text="See Planets" />
+          <Link to="/planets"><Button text="See Planets" /></Link>
         </div>
         <img src={planet} alt="" />
       </section>
